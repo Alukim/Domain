ce26f87af0d9fb098e3478982af996eefdc68792
unknown <lukaszkepka95@gmail.com>
2018-04-19 14:08:44 +0200

    added files with pseudocodw

---
+++ RateView/Fejsik.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Fejsik
+    {
+        private bool isLogged;
+
+        public bool Login(string credential)
+        {
+            return true;
+        }
+
+        public void SendRate(string comment, Meal meal)
+        {
+            if (isLogged)
+            {
+                //Send rate to fejsik
+            }
+        }
+    }
+}
---
+++ RateView/MarkController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class MarkController
+    {
+        public void SendFeedback(string comment, Meal meal, string credential)
+        {
+            MealRepository mealRepository = new MealRepository();
+
+            if (mealRepository.MealExist(meal))
+            {
+                var fejsik = new Fejsik();
+                if (fejsik.Login(credential))
+                {
+                    fejsik.SendRate(comment, meal);
+                }
+            }
+        }
+
+    }
+}
